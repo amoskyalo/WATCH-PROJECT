283924795cc795c72c9adba1305a82ef41f6a1d5
amos kyalo <amoskyalo927@gmail.com>
2022-06-25 21:19:36 +0300

    added the footer, watch categories and the courasel

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ http.onload = function(){
 	if(this.readyState == 4 && this.status == 200){
 		let products = JSON.parse(this.responseText);
 		let gold = products.filter(goldWatches=> goldWatches.category == "gold");
+		let diamond = products.filter(diamondWatches => diamondWatches.category == "diamond");
 		let output1 = "";
+		let output2 ="";
 		for(let item of gold){
 			output1 += `
 				<div class="product">
@@ -23,11 +25,28 @@ http.onload = function(){
 				</div>
 			`;
 		}
-	document.querySelector(".gold-watch").innerHTML = output1;
+		for(let item of diamond){
+			output2 += `
+			<div class="product">
+					<img src="${item.image}" alt="${item.description}">
+					<p class="title">${item.title}</p>
+					<p class="description">${item.description}</p>
+					<p class="price">
+						<span>Kshs.</span>
+						<span>${item.price}</span>
+					</p>
+					<p class="discount"><span>save.</span>
+					     <span>${item.discount}</span>
+					<p class="cart">Add to cart <i class="fa fa-cart-plus" aria-hidden="true"></i></p>
+				</div>
+			`;
+		}
+		document.querySelector(".diamond").innerHTML = output2;
+	document.querySelector(".gold").innerHTML = output1;
 	}
 } 
 
-
+//dropdowns
 function myFunction() {
 	document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -44,10 +63,18 @@ function myFunction() {
 	}
   }
 
-const data = ["Amos", "Kyalo", "Mwangangi", "Busses"];
+//courasel
+const slider = document.querySelector('.slider');
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
 
-const dataLength = data.filter(dataLengthOutput=>{
-	if(dataLengthOutput.length == 9){
-		return dataLengthOutput;
-	};
+sectionIndex = 0;
+
+leftArrow.addEventListener('click', function(){
+	sectionIndex = (sectionIndex > 0) ? sectionIndex -1: 0;
+	slider.style.transform = 'translate('+(sectionIndex)* -16 +'%)';
+})
+rightArrow.addEventListener('click', function(){
+	sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 :2;
+	slider.style.transform = 'translate('+(sectionIndex) * -16 +'%)';
 });
