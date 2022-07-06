81f2f33c67cdb4b7383c05ebaf02953c3a15b82b
amoskyalo <amoskyalo927@gmail.com>
2022-07-06 09:12:18 -0700

    fixed some minor changes

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ http.onload = function(){
 		let products = JSON.parse(this.responseText);
 		let gold = products.filter(goldWatches=> goldWatches.category == "gold");
 		let diamond = products.filter(diamondWatches => diamondWatches.category == "diamond");
+		let silver = products.filter(silverWatches => silverWatches.category == "silver");
 		let output1 = "";
-		let output2 ="";
+		let output2 = "";
+		let output3 = "";
 		for(let item of gold){
 			output1 += `
 				<div class="product">
@@ -41,8 +43,27 @@ http.onload = function(){
 				</div>
 			`;
 		}
+		for(let item of silver){
+			output3+= `
+			<div class="product">
+			<img src="${item.image}" alt="${item.description}">
+			<p class="title">${item.title}</p>
+			<p class="description">${item.description}</p>
+			<p class="price">
+				<span>Kshs.</span>
+				<span>${item.price}</span>
+			</p>
+			<p class="discount"><span>save.</span>
+				 <span>${item.discount}</span>
+			<p class="cart">Add to cart <i class="fa fa-cart-plus" aria-hidden="true"></i></p>
+		</div>
+			`
+		}
+		let length = silver.length;
+		console.log(length, silver);
+		document.querySelector(".gold").innerHTML = output1;
 		document.querySelector(".diamond").innerHTML = output2;
-	document.querySelector(".gold").innerHTML = output1;
+		document.querySelector(".silver").innerHTML = output3;
 	}
 } 
 
@@ -75,6 +96,52 @@ leftArrow.addEventListener('click', function(){
 	slider.style.transform = 'translate('+(sectionIndex)* -16 +'%)';
 })
 rightArrow.addEventListener('click', function(){
-	sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 :2;
+	sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 :3;
 	slider.style.transform = 'translate('+(sectionIndex) * -16 +'%)';
+});
+
+//courasel 2
+const slider2 = document.querySelector('.slider2');
+const leftArrow2 = document.querySelector('.left2');
+const rightArrow2 = document.querySelector('.right2');
+
+sectionIndex = 0;
+
+leftArrow2.addEventListener('click', function(){
+	sectionIndex = (sectionIndex > 0) ? sectionIndex -1: 0;
+	slider2.style.transform = 'translate('+(sectionIndex)* -16 +'%)';
+})
+rightArrow2.addEventListener('click', function(){
+	sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 :3;
+	slider2.style.transform = 'translate('+(sectionIndex) * -16 +'%)';
+});
+
+//courasel 3
+const slider3 = document.querySelector('.slider3');
+const leftArrow3 = document.querySelector('.left3');
+const rightArrow3 = document.querySelector('.right3');
+
+sectionIndex = 0;
+
+leftArrow3.addEventListener('click', function(){
+	let sectionLength = 
+	sectionIndex = (sectionIndex > 0) ? sectionIndex -1: 0;
+	slider3.style.transform = 'translate('+(sectionIndex)* -16 +'%)';
+})
+rightArrow3.addEventListener('click', function(){
+	sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 :3;
+	slider3.style.transform = 'translate('+(sectionIndex) * -16 +'%)';
+});
+
+//corasel 4
+
+//number range
+var range = document.querySelector('.inputRange');
+var field = document.getElementById('num1');
+
+range.addEventListener('input', function(user_value){
+	field.value = user_value.target.value;
+});
+field.addEventListener('input', function(user_value){
+	range.value = user_value.target.value;
 });
